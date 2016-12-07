c25f042a9dbf6e9104d0e7f47f49cdfc4d5a636a
Gempar Karoban <gemparkaroban@users.noreply.github.com>
2016-12-08 00:36:42 +0700

    Update buvina.js

--- javascripts/buvina.js
+++ javascripts/buvina.js
@@ -1,7 +1,9 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
+<script type="text/javascript">
+  import React from 'react';
+  import ReactDOM from 'react-dom';
 
-ReactDOM.render(
-  <h1>Hello, world!</h1>,
-  document.getElementById('root')
-);
+  ReactDOM.render(
+    <h1>Hello, world!</h1>,
+    document.getElementById('root')
+  );
+</script>
